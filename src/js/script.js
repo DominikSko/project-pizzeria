7ac028a0f95362bfe768d6694d4c3667dd93f412
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-04-15 10:20:59 +0200

    7.5 price buttons working

--- src/js/script.js
+++ src/js/script.js
@@ -154,50 +154,52 @@
       });
 
     }
+
     processOrder(){
       const thisProduct = this;
 
-      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+      // read all data from the form (using utils.serializeFormToObject) and save it to const formData
       const formData = utils.serializeFormToObject(thisProduct.form);
       //console.log('formData', formData);
 
       thisProduct.params = {}; // dlaczego tworzymy obiekt? nie ma go w algorytmie
 
-      /* set variable price to equal thisProduct.data.price */
+      // set variable price to equal thisProduct.data.price
       let price = thisProduct.data.price;
       console.log(price);
 
-      /* START LOOP: for each paramId in thisProduct.data.params */
+      // START LOOP: for each paramId in thisProduct.data.params
       for(let paramId in thisProduct.data.params) {
         const param = thisProduct.data.params[paramId]; // save the element in thisProduct.data.params with key paramId as const param
         console.log('param:',param);
 
-        /* START LOOP: for each optionId in param.options */
+        // START LOOP: for each optionId in param.options
         for(let optionId in param.options){
           const option = param.options[optionId];  // save the element in param.options with key optionId as const option
           console.log('option', option);
 
 
           const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
-          /* START IF: if option is selected and option is not default */
+          // START IF: if option is selected and option is not default
           if(optionSelected && !option.default){
 
-            /* add price of option to variable price */
+            // add price of option to variable price
             price += option.price;
+          }
+          // START ELSE IF: if option is not selected and option is default
+          else if(!optionSelected && option.default){
 
-          /* START ELSE IF: if option is not selected and option is default */
-          } else if(!optionSelected && option.default){
-
-            /* deduct price of option from price */
+            // deduct price of option from price
             price -= option.price;
 
           } // END ELSE IF,
         }   // END LOOP: for each optionId in param.options
-      }     // END LOOP: for each paramId in thisProduct.data.params */
+      }     // END LOOP: for each paramId in thisProduct.data.params
 
-      /* set the contents of thisProduct.priceElem to be the value of variable price */
-      thisProduct.priceElem.innerHTML = thisProduct.price;
+      // set the contents of thisProduct.priceElem to be the value of variable price
+      thisProduct.priceElem.innerHTML = price;
     }
+
   }
 
   const app = {      // obiekt który pomoże nam w organizacji kodu naszej aplikacji
