902cfd86c705be1af58c9c1ab2f94776726be7c6
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-04-29 23:17:25 +0200

    hour picker and date picker working

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -2,6 +2,7 @@
 import {select, templates} from '../settings.js';
 import {AmountWidget} from './AmountWidget.js';
 import {DatePicker} from './DatePicker.js';
+import {HourPicker} from './HourPicker.js';
 
 export class Booking {
   constructor(element) {
@@ -31,6 +32,7 @@ export class Booking {
     // analogicznie do peopleAmount znaleźć i zapisać element dla hoursAmount.
     thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
     thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
 
   }
   initWidgets(){
@@ -41,5 +43,7 @@ export class Booking {
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+
   }
 }
